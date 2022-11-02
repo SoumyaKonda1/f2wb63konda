ee0e21f1a0bf52b23627d85bf47322274b40d49a
soumyak <soumyakonda048@gmail.com>
2022-11-02 01:58:30 -0500

    changes added

--- routes/music.js
+++ routes/music.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET music page. */
 router.get('/', function(req, res, next) {
   res.render('Music', { title: 'Search Results Music' });
 });
